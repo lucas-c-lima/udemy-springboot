aae19e2f4cff4e11fda85dc032d4c028e790889d
Lucas Cordeiro <lucascordeirodelima12@gmail.com>
2025-02-06 10:39:57 -0300

    spring project creation

---
+++ curso-udemy/src/main/java/com/proj/curso_udemy/CursoUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.proj.curso_udemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemyApplication.class, args);
+	}
+
+}
---
+++ curso-udemy/src/test/java/com/proj/curso_udemy/CursoUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.curso_udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoUdemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
